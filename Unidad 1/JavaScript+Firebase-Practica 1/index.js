f3ca75234ee078c30a0c92fea9ca02a329b208cf
AnaChavezC <ana.chavez16@tectijuana.edu.mx>
2020-10-19 15:32:40 -0700

    Commit actualizacion

--- Unidad 1/JavaScript+Firebase-Practica 1/index.js
+++ Unidad 1/JavaScript+Firebase-Practica 1/index.js
@@ -1,12 +1,5 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyDdDCVNe2dC8SIiX-bd-ZRiah040sJn0rM",
-    authDomain: "practica1-bdd97.firebaseapp.com",
-    databaseURL: "https://practica1-bdd97.firebaseio.com",
-    projectId: "practica1-bdd97",
-    storageBucket: "practica1-bdd97.appspot.com",
-    messagingSenderId: "576143403142",
-    appId: "1:576143403142:web:dfdcbd71dcd4bf3a728907",
-    measurementId: "G-VK0CFFR9S8"
+    
   };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
